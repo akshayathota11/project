12edb3a3c0c6dbe1f809557bf52a45736cc294c8
athotaakshay11 <athotaakshay111@gmail.com>
2025-07-16 22:38:23 +0100

    Add cleaned dataset railway_cleaned.csv, update dataprocessing.py, and ignore empty dataprocessing file

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,1 +1,12 @@
-print("Akshay is a teacher")
+import pandas as pd
+df = pd.read_csv('railway system dataset.csv')
+print(df.head())
+print("\nColumns:", df.columns.tolist())
+df['Railcard'] = df['Railcard'].fillna('No Railcard')
+df['Reason for Delay'] = df['Reason for Delay'].fillna('No Delay')
+df = df.dropna(subset=['Actual Arrival Time'])
+print("\nMissing values:\n", df.isnull().sum())
+df.to_csv('railway_cleaned.csv', index=False)
+
+
+
