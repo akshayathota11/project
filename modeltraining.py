c5fd4d1097dbf5563618c79c55dd44e2a5bbd0e9
akshayathota11 <athotaakshay111@gmail.com>
2025-07-17 20:33:55 +0100

    Update modeltraining.py

--- modeltraining.py
+++ modeltraining.py
@@ -1,4 +1,3 @@
-# model_train.py
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -7,25 +6,25 @@ from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the cleaned dataset
+# Loading dataset
 df = pd.read_csv("railway_cleaned.csv")
 
-# Create binary target variable
+# Createing binary target variable
 df['Is_Delayed'] = df['Journey Status'].apply(lambda x: 1 if x == 'Delayed' else 0)
 
-# Convert time and date
+# Convert time and date into datetime
 df['Date of Journey'] = pd.to_datetime(df['Date of Journey'], errors='coerce')
 df['Departure Time'] = pd.to_datetime(df['Departure Time'], format='%H:%M:%S', errors='coerce')
 df['Hour'] = df['Departure Time'].dt.hour
 df['Day'] = df['Date of Journey'].dt.day_name()
 
-# Select features
+# features selection
 features = ['Departure Station', 'Arrival Destination', 'Ticket Class', 'Ticket Type', 'Railcard', 
             'Purchase Type', 'Payment Method', 'Hour', 'Day', 'Price']
 X = df[features]
 y = df['Is_Delayed']
 
-# One-hot encoding for categorical features
+# Encoding for categorical features
 X = pd.get_dummies(X)
 
 # Train-test split
@@ -52,3 +51,10 @@ plt.tight_layout()
 plt.savefig("confusion_matrix.png")
 plt.show()
 
+# Feature Importance
+importances = pd.Series(clf.feature_importances_, index=X.columns).sort_values(ascending=False).head(10)
+importances.plot(kind='barh')
+plt.title("Top 10 Important Features")
+plt.tight_layout()
+plt.savefig("feature_importance.png")
+plt.show()
