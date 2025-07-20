c1dfa982b197d9e44201fb27369d2210cb5a416b
akshayathota11 <athotaakshay111@gmail.com>
2025-07-20 23:49:26 +0100

    Update modeltraining.py

--- modeltraining.py
+++ modeltraining.py
@@ -5,38 +5,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-# Loading dataset
 df = pd.read_csv("railway_cleaned.csv")
-
-# Createing binary target variable
 df['Is_Delayed'] = df['Journey Status'].apply(lambda x: 1 if x == 'Delayed' else 0)
-
 # Convert time and date into datetime
 df['Date of Journey'] = pd.to_datetime(df['Date of Journey'], errors='coerce')
 df['Departure Time'] = pd.to_datetime(df['Departure Time'], format='%H:%M:%S', errors='coerce')
 df['Hour'] = df['Departure Time'].dt.hour
 df['Day'] = df['Date of Journey'].dt.day_name()
-
-# features selection
 features = ['Departure Station', 'Arrival Destination', 'Ticket Class', 'Ticket Type', 'Railcard', 
             'Purchase Type', 'Payment Method', 'Hour', 'Day', 'Price']
 X = df[features]
 y = df['Is_Delayed']
-
-# Encoding for categorical features
 X = pd.get_dummies(X)
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 # Train the model
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
-
-# Predict
 y_pred = clf.predict(X_test)
-
 # Evaluation
 print("Classification Report:")
 print(classification_report(y_test, y_pred))
