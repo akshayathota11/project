9942f3e87ef874750f1e2cac654f7936e91d15f6
akshayathota11 <athotaakshay111@gmail.com>
2025-07-20 23:46:12 +0100

    Update clustering4.py

--- clustering4.py
+++ clustering4.py
@@ -2,11 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-
-# Load cleaned data
 df = pd.read_csv("railway_cleaned.csv")
-
-# Add binary delay column
 df['Is_Delayed'] = df['Journey Status'].apply(lambda x: 1 if x == 'Delayed' else 0)
 
 # Feature Engineering
@@ -14,27 +10,17 @@ df['Date of Journey'] = pd.to_datetime(df['Date of Journey'], errors='coerce')
 df['Departure Time'] = pd.to_datetime(df['Departure Time'], format='%H:%M:%S', errors='coerce')
 df['Hour'] = df['Departure Time'].dt.hour
 df['Day'] = df['Date of Journey'].dt.day_name()
-
-# Select features for clustering
 features = ['Departure Station', 'Arrival Destination', 'Ticket Class', 'Ticket Type',
             'Railcard', 'Purchase Type', 'Payment Method', 'Hour', 'Day']
-
 df_cluster = pd.get_dummies(df[features])
 scaler = StandardScaler()
 scaled_features = scaler.fit_transform(df_cluster)
-
-# KMeans clustering
-kmeans = KMeans(n_clusters=4, random_state=2)
+kmeans = KMeans(n_clusters=4, random_state=2)# KMeans clustering
 df['Cluster'] = kmeans.fit_predict(scaled_features)
-
-# Cluster sizes
 print("\n Cluster Distribution:\n", df['Cluster'].value_counts().rename_axis('Cluster').to_frame('Count'))
-
-# Average delay rate per cluster
 print("\n Average Delay Rate by Cluster:")
 print(df.groupby('Cluster')['Is_Delayed'].mean().to_frame('Delay Rate'))
-
-# Save Cluster Plot
+#for saving
 plt.figure(figsize=(8, 4))
 df['Cluster'].value_counts().sort_index().plot(kind='bar', color='skyblue')
 plt.title("Number of Records per Cluster")
